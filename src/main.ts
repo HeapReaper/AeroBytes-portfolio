76ef51e3f69b1b69de3d211f8a59c09a4c5bf861
AeroBytes <kelvin@aerobytes.nl>
2025-02-27 23:41:57 +0100

    Added basic structure

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,22 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'aos/dist/aos.css';
+import 'glightbox/dist/css/glightbox.min.css';
+import 'swiper/swiper-bundle.css';
+import { Navbar } from '@components/Navbar';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+const app: HTMLElement | null = document.querySelector<HTMLDivElement>('#app');
+
+if (app) {
+  app.innerHTML = `
+    <div>
+    
     </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+  `;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+  app.appendChild(Navbar());
+} else {
+  throw new Error('Element with ID app not found in main.ts!');
+}
+
