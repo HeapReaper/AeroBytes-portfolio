857313776d34d679ca102e8b1ab5fadaf2f68d90
AeroBytes <kelvin@aerobytes.nl>
2025-02-28 19:53:33 +0100

    Added bout section

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,21 @@
-import './style.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'glightbox/dist/css/glightbox.min.css';
 import 'swiper/swiper-bundle.css';
+import './style.css'
 
 import { Navbar } from '@components/Navbar';
+import { Footer } from '@components/Footer';
 import { Hero } from '@pages/Hero.ts';
+import { About } from '@pages/About.ts';
 
 const app: HTMLElement | null = document.querySelector<HTMLDivElement>('#app');
 
 if (app) {
   app.appendChild(Navbar());
   app.appendChild(Hero());
+  app.appendChild(About());
+  app.appendChild(Footer());
 } else {
   throw new Error('Element with ID app not found in main.ts!');
 }
---
+++ src/pages/Testimonials.ts
@@ -0,0 +1,13 @@
+// src/pages/About.ts
+
+export function testimonials(): HTMLElement {
+  const testimonialsSection: HTMLElement = document.createElement('section');
+  //testimonialsSection.className = '';
+  testimonialsSection.id = 'about';
+
+  testimonialsSection.innerHTML = `
+   
+  `;
+
+  return testimonialsSection;
+}
