17357a788e437567dd9b9ac037a9d7bee82a81a7
AeroBytes <kelvin@aerobytes.nl>
2025-03-01 14:08:16 +0100

    Updated text

--- src/main.ts
+++ src/main.ts
@@ -8,13 +8,14 @@ import { Navbar } from '@components/Navbar';
 import { Footer } from '@components/Footer';
 import { Hero } from '@pages/Hero.ts';
 import { About } from '@pages/About.ts';
-
+import { Resume } from '@pages/Resume.ts';
 const app: HTMLElement | null = document.querySelector<HTMLDivElement>('#app');
 
 if (app) {
   app.appendChild(Navbar());
   app.appendChild(Hero());
   app.appendChild(About());
+  app.appendChild(Resume());
   app.appendChild(Footer());
 } else {
   throw new Error('Element with ID app not found in main.ts!');
