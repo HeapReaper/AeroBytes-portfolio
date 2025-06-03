48b007624e5c54efeab4e992bc60fe26f2c6545d
AeroBytesNL <kelvin@aerobytes.nl>
2025-06-03 20:50:35 +0200

    Added AdonisJS to tools list

--- src/pages/Tools.ts
+++ src/pages/Tools.ts
@@ -28,6 +28,7 @@ import imageCoolify from '@img/icons/coolify-logo.webp';
 import imageRedis from '@img/icons/redis.webp';
 import imageActions from '@img/icons/gh-actions.png';
 import imageDirectAdmin from '@img/icons/directadmin.svg';
+import imageAdonisJS from '@img/icons/adonisjs.png';
 
 export function Tools(): HTMLElement {
   const toolsSection: HTMLElement = document.createElement('section');
@@ -47,6 +48,10 @@ export function Tools(): HTMLElement {
       <p align="center">
         <h2 align="center">Frameworks</h2>
         <div align="center">
+          <div class="image-hover-resize-10">
+            <img src="${imageAdonisJS}" style="width: 50px;" />
+            <p>AdonisJS</p>
+          </div>
           <div class="image-hover-resize-10">
             <img src="${imageAlpineJS}" style="width: 50px;" />
             <p>AlpineJS</p>
